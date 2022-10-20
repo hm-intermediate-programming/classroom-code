620025dbd429df215d892961aac070a5b1fb659e
Ms Feng <irene_feng@horacemann.org>
2022-10-20 13:11:33 -0400

    revert paddle

--- oop_unit/JFrame/Paddle.java
+++ oop_unit/JFrame/Paddle.java
@@ -1,16 +1,4 @@
 
 public class Paddle {
-    // positions 
-    int x; // Class scope
-    int y;
-
-    public Paddle(int x, int y) { // local scope 
-        this.x = x;
-        this.y = y;
-    }
-
-    public void draw() {
-
-    }
 
 }
