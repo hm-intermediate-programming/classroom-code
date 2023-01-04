f1ecd43ead831ee41df3f95776b19aa93674819c
Ms Feng <irene_feng@horacemann.org>
2023-01-04 08:22:27 -0500

    NumberGuessGame Jar File

---
+++ arrays/NumberGuessGame/src/NumberGuessGameJar.java
@@ -0,0 +1,9 @@
+/*
+ * This class expects to import a NumberGuessGame.jar file, 
+ * and run the packaged source code as an executable.  
+ */
+public class NumberGuessGameJar {
+    public static void main(String args[]) throws Exception {
+        NumberGuessGame.main(args);
+    }
+}
